c91ba9986c81e3d8308ccad3d52652b108e24c2a
asahi-nixos <Lawrence.Matsuoka@proton.me>
2025-01-29 00:19:57 -0400

    fix code formatting in server.c

--- server.c
+++ server.c
@@ -1,11 +1,11 @@
 #include "error.h"
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // socket
 
@@ -19,40 +19,33 @@
 
 // write to client
 
-// close
-
-// Video 4
-// Error function
-//void error(const char *msg) {
-//  perror(msg);
-//  exit(1);
-//}
-
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Port number not provided. Terminating program.\n");
     exit(1);
   }
 
+  // Initialize the socket
   int sockfd, newsockfd, portnum, n;
   char buffer[255];
 
   struct sockaddr_in serv_addr, cli_addr;
   socklen_t clilen;
 
+  // IPv4
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     error("Error opening socket");
   }
 
-  bzero((char *) &serv_addr, sizeof(serv_addr));
+  bzero((char *)&serv_addr, sizeof(serv_addr));
   portnum = atoi(argv[1]);
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portnum);
 
-  if (bind(sockfd, (struct sockaddr * ) &serv_addr, sizeof(serv_addr)) < 0) {
+  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     error("Binding failed");
   }
 
@@ -60,13 +53,13 @@ int main(int argc, char *argv[]) {
   listen(sockfd, 5);
   clilen = sizeof(cli_addr);
 
-  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
   if (newsockfd < 0) {
     error("Error on accept");
   }
 
-  while(1) {
+  while (1) {
     bzero(buffer, 255);
     n = read(newsockfd, buffer, 255);
     if (n < 0) {
@@ -87,6 +80,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // Close the socket
   close(newsockfd);
   close(sockfd);
   return 0;
